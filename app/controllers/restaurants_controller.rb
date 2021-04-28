4b644aa7526ed4dcdc69a4b3903b838c24586e5e
Cindy Liang <hakkacindy@gmail.com>
2021-04-28 02:31:52 +0100

    index page done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,20 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show 
+  end
+
+  def new   
+  end
+
+  def create 
+  end
+
+  def update
+  end
+
+  def destroy 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+  #CRUD
+  # verb 'path', to: 'controller#action'
+
+  # #read all restaurants
+  # get '/restaurants', to: "restaurants#index"
+  # #read one restaurant
+  # get '/restaurants/:id', to: "restaurants#show"
+
+  # #create a restaurant
+  # get '/restaurants/new', to: 'restaurants#new'
+  # post '/restaurants', to: 'restaurant#create'
+
+  # #update a restaurant
+  # get '/restaurant/:id/edit', to: 'restaurant#edit'
+  # patch '/restaurant/:id', to: 'restaurant#update'
+
+  # #delete a restaurant  
+  # delete '/restaurant/:id', to: 'restaurant#destroy'
+
+  resources :restaurants
 end
